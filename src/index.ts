29036a70da568e6d530d9fb584e06a48f2e113da
Wesley Banfield <wesleybanfield@gmail.com>
2018-08-28 11:13:19 +1200

    Showing submenu text

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,13 @@ import {
 import {
     Menu
 } from '@phosphor/widgets';
-import { JSONObject } from '@phosphor/coreutils';
 
 import '../style/index.css';
 
 
 // Names of commands that will be used (fro jupyterlab)
 namespace CommandIDs {
-    export const test = 'helpmenu:test';
-    export const test2 = 'helpmenu:test2';
+    export const addDocumentation = 'helpmenu:addDocumentation';
 }
 
 /**
@@ -27,41 +25,32 @@ const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab-documentation-extension',
   autoStart: true,
   requires: [IMainMenu],
-  activate : activateTEST,
+  activate : activate,
 };
 
-function activateTEST (app: JupyterLab, mainmenu: IMainMenu) {
-    console.log('JupyterLab extension jupyterlab-documentation-extension is activated!');
+function activate (app: JupyterLab, mainmenu: IMainMenu) {
+    console.log('JupyterLab extension jupyterlab-documentation-extension is activated!!');
     const { commands } = app; // Pulling everything out of app
     
-    const testMenu = new Menu({ commands });
-    commands.addCommand(CommandIDs.test, {
-        label : 'test',
-        caption : 'This is a test item',
-        execute: () => {
-            console.log("1");
+    const generatedDocumentationMenu = new Menu({ commands });
+    generatedDocumentationMenu.title.label = "Genererated Documentation";
+    commands.addCommand(CommandIDs.addDocumentation, {
+    label: args => {
+    	let title = args['label'] as string;
+	return title
+     },
+        execute: args => {
+	const key = args['label'];
+	return console.log(key);
         }
     });
-    commands.addCommand(CommandIDs.test2, {
-        execute: () => {
-            console.log("2");
-        }
-    });
-    testMenu.title.label = "Genererated Documentation";
-    let command = 'helpMenu:test';
-    let args: JSONObject = {
-        name : 'Add',
-        label : 'add',
-    }
-   testMenu.addItem({ command, args   });
-       testMenu.addItem({
-       command : CommandIDs.test2,
-       args: {name : 'Add2', label : 'add2'}
-   });
+    generatedDocumentationMenu.addItem({ command : CommandIDs.addDocumentation, args: {'label' : 'test'}});
+    generatedDocumentationMenu.addItem({ command : CommandIDs.addDocumentation, args: {'label' : 'test1'}});
+   
   // Add the command to the palette.
   mainmenu.helpMenu.addGroup(
       [
-          {type : 'submenu' as Menu.ItemType, submenu : testMenu}
+          {type : 'submenu' as Menu.ItemType, submenu : generatedDocumentationMenu}
       ],
       15
   );
