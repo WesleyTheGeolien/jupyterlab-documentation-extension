5076e2333d3605cb23d786cfe3d5167e438f6330
Wesley Banfield <wesleybanfield@gmail.com>
2018-08-28 14:45:05 +1200

    Will open matplotlib documentation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import {
-  JupyterLab, JupyterLabPlugin
+  JupyterLab, JupyterLabPlugin, ILayoutRestorer
 } from '@jupyterlab/application';
+
+import {
+  IFrame,
+  InstanceTracker,
+  MainAreaWidget,
+} from '@jupyterlab/apputils';
 
 import {
   IMainMenu
@@ -10,13 +16,26 @@ import {
     Menu
 } from '@phosphor/widgets';
 
+import { URLExt } from '@jupyterlab/coreutils';
+
 import '../style/index.css';
 
 
 // Names of commands that will be used (fro jupyterlab)
 namespace CommandIDs {
-    export const addDocumentation = 'helpmenu:addDocumentation';
+    export const addDocumentation = 'help:addDocumentation';
+    export const open = 'customhelp:open';
 }
+
+/**
+ * A flag denoting whether the application is loaded over HTTPS.
+ */
+const LAB_IS_SECURE = window.location.protocol === 'https:';
+
+/**
+ * The class name added to the help widget.
+ */
+const HELP_CLASS = 'jp-Help';
 
 /**
  * Initialization data for the jupyterlab-documentation-extension extension.
@@ -28,24 +47,64 @@ const extension: JupyterLabPlugin<void> = {
   activate : activate,
 };
 
-function activate (app: JupyterLab, mainmenu: IMainMenu) {
+function activate (app: JupyterLab, mainmenu: IMainMenu, restorer: ILayoutRestorer) {
+    let counter = 0;  
+    const namespace = 'help-doc';
     console.log('JupyterLab extension jupyterlab-documentation-extension is activated!!');
-    const { commands } = app; // Pulling everything out of app
+    const { commands, shell } = app; // Pulling everything out of app
+    const tracker = new InstanceTracker<MainAreaWidget>({ namespace });
+
+ 
+
+    /**
+     * Create a new HelpWidget widget.
+     */
+    function newHelpWidget(url: string, text: string): MainAreaWidget {
+      let content = new IFrame();
+      content.url = url;
+      content.addClass(HELP_CLASS);
+      content.title.label = text;
+      content.id = `${namespace}-${++counter}`;
+      let widget = new MainAreaWidget({ content });
+      widget.addClass('jp-Help');
+      return widget;
+    }
+
     
     const generatedDocumentationMenu = new Menu({ commands });
     generatedDocumentationMenu.title.label = "Genererated Documentation";
+
     commands.addCommand(CommandIDs.addDocumentation, {
     label: args => {
     	let title = args['label'] as string;
-	return title
+    	return title
      },
         execute: args => {
-	const key = args['label'];
-	return console.log(key);
+	        const key = args['label'];
+	        return console.log(key);
         }
     });
+    
+      commands.addCommand(CommandIDs.open, {
+    label: args => args['text'] as string,
+    execute: args => {
+      const url = args['url'] as string;
+      const text = args['text'] as string;
+
+      // If help resource will generate a mixed content error, load externally.
+      if (LAB_IS_SECURE && URLExt.parse(url).protocol !== 'https:') {
+        window.open(url);
+        return;
+      }
+
+      let widget = newHelpWidget(url, text);
+      tracker.add(widget);
+      shell.addToMainArea(widget);
+    }
+  });
+  
     generatedDocumentationMenu.addItem({ command : CommandIDs.addDocumentation, args: {'label' : 'test'}});
-    generatedDocumentationMenu.addItem({ command : CommandIDs.addDocumentation, args: {'label' : 'test1'}});
+    generatedDocumentationMenu.addItem({ command : CommandIDs.open, args: {'text' : 'test1', 'url':'https://matplotlib.org/contents.html'}});
    
   // Add the command to the palette.
   mainmenu.helpMenu.addGroup(
